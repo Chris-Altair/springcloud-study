2584efa5a5c9dfa8dfd7b8f610d03fd6741a7fe7
Chris-Altair <2777128467@qq.com>
2019-08-15 17:20:01 +0800

    测试bus

--- service-provider/src/main/java/pers/cloud/serviceprovider/ServiceProviderApplication.java
+++ service-provider/src/main/java/pers/cloud/serviceprovider/ServiceProviderApplication.java
@@ -2,9 +2,11 @@ package pers.cloud.serviceprovider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ServiceProviderApplication {
     private static Logger logger = LoggerFactory.getLogger(ServiceProviderApplication.class);
-
+    @Value("${test-bus}")
+    private String testBus;
     @GetMapping("/welcome")
     public String service(@RequestParam(name = "name") String name) {
         logger.info("name:{}", name);
-        String result = "Hello " + name + "!";
+        String result = "Hello " + name + "! "+testBus;
         return result;
     }
 
